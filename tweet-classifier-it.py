61eeb78e0bc3feb4c19cad1fe1025c5bdf68fa89
Ray <ray.ruisun@qq.com>
2021-02-22 16:55:23 +0000

    add iso3166

--- tweet-classifier-it.py
+++ tweet-classifier-it.py
@@ -2,13 +2,12 @@ from procstream import StreamProcessMicroService
 import os
 import logging as logger
 import spacy
-# import pycountry
+from country_bboxes import get_ISO3166_citycode
 from geopy.geocoders import Nominatim
 from geoextract import GeoLookup, osm_lookup_place
 
 config = {"MODULE_NAME": os.environ.get('MODULE_NAME', 'LEWS_LANG_DETECT'),
           "CONSUMER_GROUP": os.environ.get("CONSUMER_GROUP", "LEWS_LANG_DETECT_CG")}
-
 
 class StreamProcessClassifyItalianTweets(StreamProcessMicroService):
     def __init__(self, config_new, italian_landslip_model, italian_rain_model, geo_lookup_object):
@@ -19,13 +18,10 @@ class StreamProcessClassifyItalianTweets(StreamProcessMicroService):
 
     def process_message(self, message):
         print("开始process  意大利数据")
-        print(message)
         payload = message.value
         if payload.get("lews_meta_detected_lang") == "it" \
                 and payload.get("lang") == "it":
             payload["lews-meta-it_class_flag"] = "True"
-            print('----------')
-            print(payload)
             payload = self.geo_extraction(payload)
             payload = self.classify_landslip(payload)
             payload = self.classify_rain(payload)
@@ -96,7 +92,7 @@ class StreamProcessClassifyItalianTweets(StreamProcessMicroService):
 
         # TODO 同样需要多种类型判断
         if type(data["text"]) is not float:
-            cleaned_text = self.data_clean(row["text"])
+            cleaned_text = self.data_clean(data["text"])
             extracted = self.geo_lookup_object.get_geotag(cleaned_text)
             duplicate_removed = self.remove_duplicate(extracted)
             valid_places = self.country_filter(duplicate_removed)
@@ -122,17 +118,16 @@ class StreamProcessClassifyItalianTweets(StreamProcessMicroService):
                     print(location.address)
 
                     data['lews-meta-it_location_address'] = location.address
+                    data['lews-meta-it_location_address_city'] = location.raw["address"]["city"]
+                    data['lews-meta-it_location_address_city_district'] = location.raw["address"]["city_district"]
+                    data['lews-meta-it_location_address_city_ISO3166-2_with_country'] = get_ISO3166_citycode("IT-"+location.raw["address"]["city"])
+                    data['lews-meta-it_location_address_city_ISO3166-2'] = get_ISO3166_citycode(location.raw["address"]["city"])
 
-                    # tweet_record['lews-meta-it_location_ISO3166-2'] = pycountry.countries.get(
-                    #     alpha_2=valid_places[0]).alpha_2
-                    #
-                    # logger.debug("tweet_record['lews-meta-it_location_ISO3166-2'] : ",
-                    #              tweet_record['lews-meta-it_location_ISO3166-2'])
 
                 except:
                     print('not found')
 
-        return
+        return data
 
     # def detection(self, model, new_col, data):
     #
@@ -163,12 +158,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    # country_code = pycountry.countries.get(name="Shanghai")
-    #
-    # print(country_code)
-    #
-    # geolocator = Nominatim(user_agent='myuseragent')
-    # location = geolocator.reverse("53.339688, -6.236688")
-    #
-    # print("location address : ")
-    # print(location.raw)
